c0c9c114c01b0841da01a274b8967ac960b7c431
Tanish Oberoi <tanishhacks@gmail.com>
2026-03-18 21:05:59 +0530

    Solved: Passing Exams
    Chef gave a series of 
    3
    3 exams, and scored 
    𝑋
    X, 
    𝑌
    Y and 
    𝑍
    Z in them respectively, out of 
    100
    100 marks each.
    
    Chef needs to get 
    50
    50 or more marks in at least 
    2
    2 out of 
    3
    3 exams to pass. Determine if Chef passed.
    
    Input Format
    The first line of input will contain a single integer 
    𝑇
    T, denoting the number of test cases.
    The first line contains 
    3
    3 integers - 
    𝑋
    X, 
    𝑌
    Y and 
    𝑍
    Z.
    Output Format
    
    For each test case, output on a new line 
    Yes
    Yes or 
    No
    No depending on whether Chef passed or not.
    
    Constraints
    1
    ≤
    𝑇
    ≤
    100
    1≤T≤100
    0
    ≤
    𝑋
    ,
    𝑌
    ,
    𝑍
    ≤
    100
    0≤X,Y,Z≤100
    Sample 1:
    Input
    Output
    4
    50 50 0
    15 100 100
    40 40 100
    0 0 0
    
    Yes
    Yes
    No
    No
    Explanation:
    
    Test Case 1: Chef scored 
    50
    50 or more marks in the first and second exams, therefore he passed.
    
    Test Case 3 : Chef scored 
    50
    50 or more in only the third exam. Therefore, he did not pass. (PASSEXAMS)

--- PASSEXAMS-Passing-ExamsChef-gave-a-series-of-33-exams,-and-scored-𝑋X,-𝑌Y-and-𝑍Z-in-them-respectively,-out-of-100100-marks-each.Chef-needs-to-get-5050-or-more-marks-in-at-least-22-out-of-33-exams-to-pass.-Determine-if-Chef-passed.Input-FormatThe-first-line-of-input-will-contain-a-single-integer-𝑇T,-denoting-the-number-of-test-cases.The-first-line-contains-33-integers---𝑋X,-𝑌Y-and-𝑍Z.Output-FormatFor-each-test-case,-output-on-a-new-line-YesYes-or-NoNo-depending-on-whether-Chef-passed-or-not.Constraints1≤𝑇≤1001≤T≤1000≤𝑋,𝑌,𝑍≤1000≤X,Y,Z≤100Sample-1:InputOutput450-50-015-100-10040-40-1000-0-0YesYesNoNoExplanation:Test-Case-1:-Chef-scored-5050-or-more-marks-in-the-first-and-second-exams,-therefore-he-passed.Test-Case-3-:-Chef-scored-5050-or-more-in-only-the-third-exam.-Therefore,-he-did-not-pass./Passing-ExamsChef-gave-a-series-of-33-exams,-and-scored-𝑋X,-𝑌Y-and-𝑍Z-in-them-respectively,-out-of-100100-marks-each.Chef-needs-to-get-5050-or-more-marks-in-at-least-22-out-of-33-exams-to-pass.-Determine-if-Chef-passed.Input-FormatThe-first-line-of-input-will-contain-a-single-integer-𝑇T,-denoting-the-number-of-test-cases.The-first-line-contains-33-integers---𝑋X,-𝑌Y-and-𝑍Z.Output-FormatFor-each-test-case,-output-on-a-new-line-YesYes-or-NoNo-depending-on-whether-Chef-passed-or-not.Constraints1≤𝑇≤1001≤T≤1000≤𝑋,𝑌,𝑍≤1000≤X,Y,Z≤100Sample-1:InputOutput450-50-015-100-10040-40-1000-0-0YesYesNoNoExplanation:Test-Case-1:-Chef-scored-5050-or-more-marks-in-the-first-and-second-exams,-therefore-he-passed.Test-Case-3-:-Chef-scored-5050-or-more-in-only-the-third-exam.-Therefore,-he-did-not-pass..py
+++ PASSEXAMS-Passing-ExamsChef-gave-a-series-of-33-exams,-and-scored-𝑋X,-𝑌Y-and-𝑍Z-in-them-respectively,-out-of-100100-marks-each.Chef-needs-to-get-5050-or-more-marks-in-at-least-22-out-of-33-exams-to-pass.-Determine-if-Chef-passed.Input-FormatThe-first-line-of-input-will-contain-a-single-integer-𝑇T,-denoting-the-number-of-test-cases.The-first-line-contains-33-integers---𝑋X,-𝑌Y-and-𝑍Z.Output-FormatFor-each-test-case,-output-on-a-new-line-YesYes-or-NoNo-depending-on-whether-Chef-passed-or-not.Constraints1≤𝑇≤1001≤T≤1000≤𝑋,𝑌,𝑍≤1000≤X,Y,Z≤100Sample-1:InputOutput450-50-015-100-10040-40-1000-0-0YesYesNoNoExplanation:Test-Case-1:-Chef-scored-5050-or-more-marks-in-the-first-and-second-exams,-therefore-he-passed.Test-Case-3-:-Chef-scored-5050-or-more-in-only-the-third-exam.-Therefore,-he-did-not-pass./Passing-ExamsChef-gave-a-series-of-33-exams,-and-scored-𝑋X,-𝑌Y-and-𝑍Z-in-them-respectively,-out-of-100100-marks-each.Chef-needs-to-get-5050-or-more-marks-in-at-least-22-out-of-33-exams-to-pass.-Determine-if-Chef-passed.Input-FormatThe-first-line-of-input-will-contain-a-single-integer-𝑇T,-denoting-the-number-of-test-cases.The-first-line-contains-33-integers---𝑋X,-𝑌Y-and-𝑍Z.Output-FormatFor-each-test-case,-output-on-a-new-line-YesYes-or-NoNo-depending-on-whether-Chef-passed-or-not.Constraints1≤𝑇≤1001≤T≤1000≤𝑋,𝑌,𝑍≤1000≤X,Y,Z≤100Sample-1:InputOutput450-50-015-100-10040-40-1000-0-0YesYesNoNoExplanation:Test-Case-1:-Chef-scored-5050-or-more-marks-in-the-first-and-second-exams,-therefore-he-passed.Test-Case-3-:-Chef-scored-5050-or-more-in-only-the-third-exam.-Therefore,-he-did-not-pass..py
@@ -1,14 +1,15 @@
 # cook your dish here
 test = int(input())
 for _ in range(test):
-    first, second, third = map(int, input().split())
+    lst = list(map(int, input().split()))
     
-    if first >= 50 and second >= 50:
-        print("Yes")
-    elif first >= 50 and third >=0:
-        print("Yes")
-    elif second >= 50 and third >= 50:
+    counter = 0
+    
+    for i in lst:
+        if i >= 50:
+            counter += 1
+    
+    if counter > 1:
         print("Yes")
     else:
         print("No")
-        
